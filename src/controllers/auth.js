4f621dc26241a685a08532c904740c2a5f28c892
YTRodi <yagorodi@gmail.com>
2021-02-02 19:44:14 -0300

    add get operations

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -84,7 +84,14 @@ const loginUser = async (req, res = response) => {
 			name: user.name,
 			token,
 		});
-	} catch (error) {}
+	} catch (error) {
+		console.log(chalk.bgRed(error));
+
+		res.status(500).json({
+			ok: false,
+			msg: 'Please talk to the administrator.',
+		});
+	}
 };
 
 const revalidateToken = async (req, res = response) => {
--- src/controllers/operations.js
+++ src/controllers/operations.js
@@ -1,16 +1,66 @@
 const { response } = require('express');
-const { Operation } = require('../database/database');
+const chalk = require('chalk');
+const { User, Operation } = require('../database/database');
 
-const getOperations = (req, res = response) => {
-	res.json({
-		msg: 'List',
-	});
+const getOperations = async (req, res = response) => {
+	try {
+		// Match table operations with users
+		const results = await Operation.findAll({
+			include: [{ model: User }],
+		});
+
+		const resultObj = results.map((operation) => {
+			const { User } = operation;
+
+			return {
+				id: operation.id,
+				concept: operation.concept,
+				amount: operation.amount,
+				creation: operation.creation,
+				type: operation.type,
+				user: {
+					id: User.id,
+					name: User.name,
+				},
+			};
+		});
+
+		res.status(200).json({
+			ok: true,
+			operations: resultObj,
+		});
+	} catch (error) {
+		console.log(chalk.bgRed(error));
+
+		res.status(500).json({
+			ok: false,
+			msg: 'Please talk to the administrator.',
+		});
+	}
 };
 
-const createOperation = (req, res = response) => {
-	res.json({
-		msg: 'create operation',
-	});
+const createOperation = async (req, res = response) => {
+	try {
+		let operation = {
+			...req.body,
+			user_id: req.uid,
+			creation: new Date(),
+		};
+
+		let newOperation = await Operation.create(operation);
+
+		res.status(201).json({
+			ok: true,
+			newOperation,
+		});
+	} catch (error) {
+		console.log(chalk.bgRed(error));
+
+		res.status(500).json({
+			ok: false,
+			msg: 'Please talk to the administrator.',
+		});
+	}
 };
 
 const updateOperation = (req, res = response) => {
